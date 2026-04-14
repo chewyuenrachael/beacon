954ca1ef4433fdfde797f92f15f1e83f6bd05a06
Rachael Chew <rachaelchewyuen@gmail.com>
2026-04-14 10:20:14 +0800

    feat: update outreach new page

--- app/dashboard/outreach/new/page.tsx
+++ app/dashboard/outreach/new/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { createServerComponentClient } from "@/lib/supabase";
+import { createServerComponentClient } from "@/lib/supabase-server";
 import { NewOutreachForm } from "./NewOutreachForm";
 
 export default async function NewOutreachPage() {
